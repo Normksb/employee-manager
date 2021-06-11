aa7fd45fd55fd76e98216c7ff6433a9224b3ed26
Norman Bernard <nksb414@gmail.com>
2021-06-11 05:52:16 +0000

    added functions for add employee and add role

--- index.js
+++ index.js
@@ -81,6 +81,213 @@ console.table(rows);
 return await rows;
 }
 
+function addDepartment() {
+  _sp();
+  listDepartments().then((departments) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you want to add? Enter here:",
+        name: "department",
+      },
+    ])
+    .then((answer) => {
+      if(answer.department == null || answer.department == ""){
+        printError('Sorry, you did not enter any name');
+        addDepartment();
+      } else {
+        conn.query(
+          "INSERT into departments (`dept_name`) VALUES (?)",
+          [answer.department],
+          function (err, results, fields) {
+            if (err) {
+              console.error(err.message);
+            } else {
+              listDepartments().then((deps) => {
+                console.log('Added department "%s"!', answer.department);
+                restart();
+              })
+            }
+          }
+        )
+      } 
+    });  
+  });
+}
+
+function addRole() {
+  _sp();
+  listRoles().then((roles) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the name of the role you want to add? Enter here:",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is the salary amount for this role? Enter (numbers only) here:",
+        name: "salary",
+      },
+      {
+        type: "list",
+        message: "To which department does this role belong? Enter here:",
+        choices: [
+          "1",
+          "2",
+          "3",
+        ],
+        name: "department",
+      }
+    ])
+    .then((answers) => {
+      if(answers.title == null ||
+         answers.title == "" ||
+         answers.salary == null ||
+         answers.salary == ""){
+           printError('You did not enter a title and/or salary');
+           addRole();
+         } else {
+           conn.query(
+             "INSERT into roles (`title`, `salary`, `department_id`) VALUES (?, ?, ?)",
+             [answers.title, answers.salary, answers.department],
+             function (err, results, fields) {
+              if (err) {
+                console.error(err.message);
+              } else {
+                listRoles().then((role) => {
+                  console.log('Added role "%s"!', answers.title);
+                  restart();
+                })
+              }
+            })
+         }
+    })
+  });
+}
+
+function addEmployee() {
+  _sp();
+  listEmployees().then((employees) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the first name of the employee you want to add? Enter here:",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the last name of the employee you want to add? Enter here:",
+        name: "lastName",
+      },
+      {
+        type: "list",
+        message: "Which role do you want to assign to this employee? Enter here:",
+        choices: [
+          "1",
+          "2",
+          "3",
+        ],
+        name: "roleId",
+      },
+      {
+        type: "list",
+        message: "Who is this employees manager? Enter here:",
+        choices: [
+          "1",
+          "3",
+          "5",
+        ],
+        name: "managerId",
+      }
+    ])
+    .then((answers) => {
+      if(answers.firstName == null ||
+         answers.firstName == "" ||
+         answers.lastName == null ||
+         answers.lastName == ""){
+           printError('You didnt enter a valid first name and/or last name');
+           addEmployee();
+         } else {
+           conn.query(
+             "INSERT into employees (`first_name`, `last_name`, `role_id`, `manager_id`) VALUES (?, ?, ?, ?)",
+             [answers.firstName, answers.lastName, answers.roleId, answers.managerId],
+             function (err, results, fields) {
+              if (err) {
+                console.error(err.message);
+              } else {
+                listEmployees().then((role) => {
+                  console.log('Added employee "%s"!', answers.firstName + answers.lastName);
+                  restart();
+                })
+              }
+            })
+         }
+    })
+  });
+}
+
+function updateRoles() {
+  _sp();
+  listRoles().then((roles) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "Choose the role_id of role you wish to update. Enter here:",
+        name: "role_id"
+      },
+    ]).then((answer) => {
+      //console.log(roles[answer.role_id].title);
+      var role_id = answer.role_id;
+      var oldvalues = roles[role_id - 1];
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Edit the title:",
+          name: "title",
+          default: oldvalues.title
+        },
+        {
+          type: "input",
+          message: "Edit the salary:",
+          name: "salary",
+          default: oldvalues.salary
+        },
+        {
+          type: "input",
+          message: "Edit the Department ID:",
+          name: "department_id",
+          default: oldvalues.department_id
+        }
+      ]).then((answers) => {
+        var newvalues = answers;
+        console.log('Old values:');
+        console.table(oldvalues);
+        console.log('New values:');
+        console.table(newvalues);
+        inquirer.prompt([
+          {
+            type: "confirm",
+            message: "Are you sure you want to save the new values?",
+            default: 0,
+            name: "confirmation",
+          }
+        ]).then((answer) => {
+          if(answer.confirmation){
+            _sp();
+            console.log('Values Saved!');
+            conn.query(
+              "UPDATE roles SET `title` = ?, `salary` = ?, `department_id` = ? WHERE `role_id` = ?", 
+              [newvalues.title, newvalues.salary, newvalues.department_id, role_id]
+            );
+          }
+          listRoles().then(restart);
+        })
+      });
+    });
+  });
+}
+
 // add a couple of lines of space between prompts
 function _sp(){
   console.log('');
